3ba6547b54086cbe471ebac1bfe4857ad2851aec
anna3bars <154560345+anna3bars@users.noreply.github.com>
2024-07-26 23:37:36 +0400

    disable scrolling body when the toolbar is open in mobile version

--- js/header.js
+++ js/header.js
@@ -3,7 +3,8 @@ let sp1 = document.getElementsByClassName('sp1')[0]
 let sp2 = document.getElementsByClassName('sp2')[0]
 let sp3 = document.getElementsByClassName('sp3')[0]
 let routing = document.getElementsByClassName('routing')[0]
-
+let body = document.getElementsByTagName('body')[0]
+console.log(body);
 let state = false;
 
 // sp1[0].classList.add('nerv')
@@ -15,12 +16,13 @@ hamburg.addEventListener("click", function handelHamburg()  {
   sp2.classList.add('sp2-hamburg')
   sp3.classList.add('sp3-hamburg')
   routing.classList.add('routing-mobile')
-
+  body.style.overflowY = 'hidden';
  } else {
   sp1.classList.remove('sp1-hamburg')
   sp2.classList.remove('sp2-hamburg')
   sp3.classList.remove('sp3-hamburg')
   routing.classList.remove('routing-mobile')
+  body.style.overflowY = "scroll"
  }
  
 })
